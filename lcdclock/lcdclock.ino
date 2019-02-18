459a3c621577f2887d96e4ba08031aecbe61f99b
daniel <daniel.kihn@limayrac.fr>
2019-02-18 10:20:27 +0100

    Update lcdclock.ino

--- lcdclock/lcdclock.ino
+++ lcdclock/lcdclock.ino
@@ -9,15 +9,16 @@
 
 rgb_lcd lcd; // objet LCD Grove
 
-// EEPROM decommenter la ligne du groupe   
+// eeprom DECOMMENTEZ la ligne du groupe 
 //byte adresse[]={0x01,0x00}; // groupe G1
 //byte adresse[]={0x02,0x00}; // groupe G2
 
-// Nom de l'equipe taille max 16 caracteres
+// Nom de l'equipe de taille exactement 16 caracteres
+//              0123456789ABCDEF
 char message[]="Momopop's       ";
 
 //registres DS1307   sec min heure l..d jour mois annee
-byte ds1307reg[] = {0x00,0x28,0x12,0x01,0x17,0x02,0x19};
+byte ds1307reg[] = {0x00,0x15,0x11,0x01,0x18,0x02,0x19};
 
 void setup(){
     Wire.begin();
@@ -65,12 +66,12 @@ void loop(){
     //----- affichage si au moins 1 sec s'est ecoulee --------
     static byte last=0xFF;
     if( last!=ds1307reg[0] ){ last=ds1307reg[0];
+        lcd.setCursor(0, 0);
+        lcd.print(message);
         lcd.setRGB(random(255),random(255),random(255));
         lcd.setCursor(0, 1);
         lcd.print( ds1307reg[2],HEX ); lcd.print( ':');
         lcd.print( ds1307reg[1],HEX ); lcd.print( ':');
         lcd.print( ds1307reg[0],HEX ); lcd.print("   ");
-	      lcd.setCursor(0, 1);
-	      lcd.print(message);
     }//END if(last!=ds1307reg[0])
 }//END loop()
