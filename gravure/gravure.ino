72b2e69aa53b9f6ef42c15a213eec8947c6d15b1
Daniel <35780720+DaKprofSNir@users.noreply.github.com>
2023-03-07 11:49:36 +0100

    Update gravure.ino

--- gravure/gravure.ino
+++ gravure/gravure.ino
@@ -1,35 +1,40 @@
+#include <Wire.h>
 
+char message[32]="CIEL SNir";
+byte adresse[]={0x00,0x00}; // adresse du premier octet du message gravé dans l'EEPROM
 
-void setup() {
-    Serial.begin(9600);
-}
+void setup(){
+	delay(5000); // parachute
+  Serial.begin(9600);
+	Wire.begin();
+}//setup()
 
-void loop() {
-    byte i, octet;
+void loop(){
+	if(Serial.available()) switch(Serial.read()){
+		case 'L': case 'l': case 'R': case 'r' : 
+			Serial.println("Lecture dans l'EEPROM i2c");
+			LectureEEPROM(); 
+			Serial.println("message");
+			break;
+		case 'E': case 'e': case 'W': case 'w' : 
+			Serial.println("Ecriture dans l'EEPROM i2c");
+			strcpy(message,"Nouveau texte");
+			EcritureEEPROM(); 
+			break;
+	}//if(Serial)switch()
+}//loop()
   
-    //--------Lecture d'une chaine de caractères octet par octet --------
-    Serial.println("Lecture d'une chaine de caractères octet par octet :");
-    byte chaine[]="Nom de l'Equipe\0";
-    for(i=0; chaine[i]!=0; i++){
-        octet = chaine[i];
-        //
-        // ici vous pouvez envoyer octet vers l'eeprom
-        //
-        Serial.print((char)octet);
-    }
-    Serial.println("");
-
-    //---------Construction d'une chaine de caractères octet par octet-----
-	Serial.println("Construction d'une chaine de caractères octet par octet :");
-    byte Relecture[100]={0};
-    for( i=0; i<=10; i++){
-       //
-       octet = i+'a'; // ou octet lu depuis une eeprom)
-       //
-       Relecture[i++]=octet;
-    }
-    Relecture[i++]=0; // pour terminer la chaine de caracteres
-    Serial.println( (char*)Relecture);
-    
-    delay(5000);
-}
+void LectureEEPROM(){
+	Wire.beginTransmission(0x50);  						//start + @i2c avec bit écriture
+	Wire.write(adresse, sizeof(adresse) ); 		//adresse visée dedans l'EEPROM
+	Wire.endTransmission(); 							    //stop
+	Wire.requestFrom(0x50,sizeof(message));   //Start + @i2c avec bit lecture + lecture + stop 
+	for(byte i=0;i<sizeof(message);i++) message[i]=Wire.read(); // recupere le contenu du tampon	  
+}//LectureEEPROM()
+	
+void EcritureEEPROM(){
+	Wire.beginTransmission(0x50);			      //start + @i2c avec bit écriture
+	Wire.write(adresse, sizeof(adresse) );  //adresse visée dedans l'EEPROM
+	Wire.write(message, sizeof(message) );	//les octets à graver dans l'EEPROM
+	Wire.endTransmission();					        //stop
+}//EcritureEEPROM()
